c3e91e2bfdb0ff4dc0185d18aa0788bd7e723fb4
Tiep Nguyen <tiep@tiep.info>
2019-12-18 22:04:50 +0700

    add vue config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import CompositionAPI from "@vue/composition-api";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import CompositionAPI from '@vue/composition-api'
 
-Vue.use(CompositionAPI);
-Vue.config.productionTip = false;
+Vue.use(CompositionAPI)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,25 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: () => import("../views/Home.vue")
+    path: '/',
+    name: 'home',
+    component: () => import('../views/Home.vue')
   },
   {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/About.vue")
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About.vue')
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
